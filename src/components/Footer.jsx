673201555f08acdf644e05e342ac323d0245900a
Rahul <rahul20534@iiitd.ac.in>
2024-08-23 09:30:42 +0530

    this is footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,78 +1,40 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import "./Footer.css";
 
 //image
 //import BACK from "../images/footer.png"
-import LogoIMG from '../images/logo.jpeg';
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 284px;
-  width: 100%;
-  background-image: url("https://res.cloudinary.com/dtpb5qjjf/image/upload/f_auto/v1722446390/nocqzawov5pf3gs8cy0s.png");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  margin-top: 50px;
-
-  @media screen and (max-width: 1299px) {
-    
-  }
-
-  @media screen and (max-width: 991px) {
-
-
-  }
-
-  @media screen and (max-width: 640px) {
-
-  }
-
-  @media screen and (max-width: 479px) {
-    margin-top: 20px;
-    height: 200px;
-  } 
-`
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  background-color:  #000000BF;
-  flex-direction: column;
-  gap: 20px;
-
-  @media screen and (max-width: 1299px) {
-    
-  }
-
-  @media screen and (max-width: 991px) {
-
-
-  }
-
-  @media screen and (max-width: 640px) {
-
-  }
-
-  @media screen and (max-width: 479px) {
-    gap: 10px;
-    justify-content: center;
-  } 
-`
+import LogoIMG from "../images/logo.jpeg";
+import Contect from "../icons/contect.svg"
 
 
 const Footer = () => {
   return (
-    <Container>
-      <MainContainer>
-        Working on making site better will be updated soon(there are few bugs, working on it).
-        If you want to report a bug or have any suggestion? contact me at <a href="mailto:theluffymovies@gmail.com">"theluffymovies@gmail.com"</a> 
-      </MainContainer>
-    </Container>
-  )
-}
+    <>
+      <div className="upperbox">
+        <div className="lowerbox">
+          {/* This is logo section */}
+          <div className="logo">
+            <img src={LogoIMG} alt="Logo" />
+            <h1>Luffy</h1>
+          </div>
 
-export default Footer
+           {/* This is middle section*/}
+
+           <div className="information">
+            <div className="contect">
+              <img src="" alt="" />
+
+            </div>
+
+           </div>
+
+
+
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
